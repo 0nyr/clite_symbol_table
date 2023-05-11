c035ef2994c40b276e96f52c812eb515e5806ebc
0nyr <rascoussier.florian@gmail.com>
2023-05-11 18:36:37 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clite symbol table
+
+ImplementaSymboltable(asdescribedintheLecture:SyntaxC.) You can choose any suitable data structure.
---
+++ static_scoping/main.py
@@ -0,0 +1,118 @@
+from dataclasses import dataclass
+import random
+import string
+import re
+
+TEST_FILE = "/home/onyr/Documents/5passau/programing_paradigm/src/clite_symbol_table/test_files/course_example.clite"
+
+@dataclass(eq=True, frozen=True)
+class ScopeId:
+    id: str
+
+@dataclass
+class VariableDeclaration:
+    name: str
+    type: str
+    line_index: int
+
+    def __str__(self) -> str:
+        return f"<{self.name}, {self.type}, {self.line_index}>"
+
+@dataclass
+class Scope:
+    id: ScopeId
+    parent: ScopeId | None
+    variables: list[VariableDeclaration]
+
+    def __str__(self) -> str:
+        return f"{self.id.id}"
+
+# read the file line by line
+# for each line, check if it is a variable declaration or a new scope
+# if it is a variable declaration, add it to the current scope
+# if it is a new scope, add it to the current scope and make it the current scope
+# if it is the end of a scope, make the parent scope the current scope
+
+
+def variables_declaration(line: str, current_scope: Scope, line_index: int):
+    # using re, check if string is of the form "type variable_name(, variable_name)*;"
+    # for every variable name, add it to the current scope
+    potential_var_declaration = re.match(r"^\s*(int|float|char|bool)\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*(,\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*)*;$", line)
+    if potential_var_declaration is not None:
+        variables_type = potential_var_declaration.group(1)
+        variables = re.split(r"\s*,\s*", potential_var_declaration.group(2))
+        if potential_var_declaration.group(3) is not None:
+            potential_additional_variables = re.sub(r"^\s*,\s*", "", potential_var_declaration.group(3))
+            variables.extend(re.split(r"\s*,\s*", potential_additional_variables))
+        for variable in variables:
+            current_scope.variables.append(VariableDeclaration(variable, variables_type, line_index + 1))
+
+
+def generate_random_string(length: int = 5) -> str:
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for _ in range(length))
+
+def new_scope(line: str, current_scope: Scope, scopes: dict[ScopeId, Scope]) -> None | Scope:
+    potential_new_scope = re.match(r"^\s*(.*?){\s*$", line)
+    if potential_new_scope is not None:
+        scope_name: str = potential_new_scope.group(1)
+        # group 1 can be empty for unamed blocks, just add a random name to it
+        if scope_name == "":
+            scope_name = "unnamed_scope_" + generate_random_string()
+        new_scope_id = ScopeId(scope_name)
+        new_scope = Scope(new_scope_id, current_scope.id, [])
+        return new_scope
+    else:
+        return None
+
+
+def end_of_scope(line: str, current_scope: Scope) -> bool:
+    """
+    If end of scope, return the parent scope
+    Also print the scope and its variables before deleting it
+    """
+    potential_end_of_scope = re.match(r"^\s*}\s*$", line)
+    if potential_end_of_scope is not None:
+        print_scope(current_scope)
+        return True
+    else:
+        return False
+
+def print_scope(scope: Scope):
+    var_str = ", ".join([str(var) for var in scope.variables])
+    print(f"Scope ({scope}) variables:", var_str)
+
+def main():
+    print("Static symbol table generation...")
+
+    with open(TEST_FILE) as f:
+        lines = f.readlines()
+
+    global_scope_id = ScopeId("global")
+    current_scope: Scope = Scope(global_scope_id, None, [])
+    scopes: dict[ScopeId, Scope] = {
+        global_scope_id: current_scope
+    }
+
+    for i in range(len(lines)):
+        line = lines[i]
+        variables_declaration(line, current_scope, i)
+        potential_new_scope = new_scope(line, current_scope, scopes)
+        potential_end_of_scope = end_of_scope(line, current_scope)
+        
+        if potential_new_scope is not None:
+            # add new scope to the current scope
+            current_scope = potential_new_scope
+            scopes[current_scope.id] = current_scope
+        elif potential_end_of_scope:
+            if current_scope.parent is not None:
+                current_scope = scopes[current_scope.parent]
+
+    # print the remaining outermost scope
+    assert current_scope.id == global_scope_id
+    print_scope(current_scope)
+
+
+# run the main function
+if __name__ == "__main__":
+    main()
