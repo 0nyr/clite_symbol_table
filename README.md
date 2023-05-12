5cb8007ff749ff8b147556b72ac8b3d9309d4e60
0nyr <rascoussier.florian@gmail.com>
2023-05-12 11:07:24 +0200

    Refactoring Python code for static scoping. Added results in README.

--- README.md
+++ README.md
@@ -1,3 +1,76 @@
 # Clite symbol table
 
-ImplementaSymboltable(asdescribedintheLecture:SyntaxC.) You can choose any suitable data structure.
+**Subject: Implement a Symbol table (as described in the Lecture: SyntaxC.) You can choose any suitable data structure.**
+
+> From lecture notes: A **symbol table** is a data structure kept by a translator that allows it to keep track of each declared name and its binding
+
+In the following, I discuss the Clite language and 
+
+## The *Clite* language
+
+For the following exercices, we are going to reuse the simple *Clite* program defined in lecture notes by the following slides:
+
+![grammar 1](./img/clite_ebnf_1.png)
+
+![grammar 2](./img/clite_ebnf_2.png)
+
+![grammar 3](./img/clite_ebnf_3.png)
+
+Here is the example Clite code that will be used for testing. It is a direct example with minor modifications (like nested blocks) from lecture notes:
+
+`course_example.clite`
+
+```c
+int h, i;
+void B(int w) {
+    int j, k;
+    i = 2*w;
+    w = w + 1;
+}
+
+void A(int x, int y) {
+    float i, j;
+    B(h);
+    i = 3;
+    {
+        int i, j;
+        char m;
+        i = 4;
+        j = 5;
+    }
+}
+
+void main() {
+    int a, b;
+    h = 5; a = 3; b = 2;
+    A(a, b);
+    B(h);
+}
+```
+
+
+## static scoping
+
+I have implementing a simple static scoping symbol table.  The code implements a static scoping symbol table for a programming language define in the lecture (Clite). 
+
+An example of the results expected is given in lecture notes:
+
+![Clite static symbol table](./img/clite_program_static_scope_symbol_table.png)
+
+The code parses a source file line by line, identifying variable declarations and the start and end of scopes (i.e., blocks of code).
+
+> The code has been tested on Fedora 34.
+
+Here is the output for the following input *Clite* file:
+
+Output of the static scoping symbol table for the `course_example.clite` input *Clite* file:
+
+```shell
+(phdtrack-311) [onyr@kenzael programing_paradigm]$ python src/clite_symbol_table/static_scoping/main.py 
+Static symbol table generation...
+Scope (void B(int w) ) variables: <j, int, 3>, <k, int, 3>
+Scope (unnamed_scope_CIFan) variables: <i, int, 13>, <j, int, 13>, <m, char, 14>
+Scope (void A(int x, int y) ) variables: <i, float, 9>, <j, float, 9>
+Scope (void main() ) variables: <a, int, 21>, <b, int, 21>
+Scope (global) variables: <h, int, 1>, <i, int, 1>
+```
--- static_scoping/main.py
+++ static_scoping/main.py
@@ -25,7 +25,16 @@ class Scope:
     variables: list[VariableDeclaration]
 
     def __str__(self) -> str:
-        return f"{self.id.id}"
+        var_str = ", ".join([str(var) for var in self.variables])
+        return "Scope ({}) variables: {}".format(self.id.id, var_str)
+
+@dataclass
+class SymbolTable:
+    scopes: dict[ScopeId, Scope]
+
+    def __str__(self) -> str:
+        return "\n".join([str(scope) for scope in self.scopes])
+
 
 # read the file line by line
 # for each line, check if it is a variable declaration or a new scope
@@ -33,84 +42,89 @@ class Scope:
 # if it is a new scope, add it to the current scope and make it the current scope
 # if it is the end of a scope, make the parent scope the current scope
 
-
-def variables_declaration(line: str, current_scope: Scope, line_index: int):
-    # using re, check if string is of the form "type variable_name(, variable_name)*;"
-    # for every variable name, add it to the current scope
-    potential_var_declaration = re.match(r"^\s*(int|float|char|bool)\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*(,\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*)*;$", line)
-    if potential_var_declaration is not None:
-        variables_type = potential_var_declaration.group(1)
-        variables = re.split(r"\s*,\s*", potential_var_declaration.group(2))
-        if potential_var_declaration.group(3) is not None:
-            potential_additional_variables = re.sub(r"^\s*,\s*", "", potential_var_declaration.group(3))
-            variables.extend(re.split(r"\s*,\s*", potential_additional_variables))
-        for variable in variables:
-            current_scope.variables.append(VariableDeclaration(variable, variables_type, line_index + 1))
-
-
 def generate_random_string(length: int = 5) -> str:
-    letters = string.ascii_letters
-    return ''.join(random.choice(letters) for _ in range(length))
-
-def new_scope(line: str, current_scope: Scope, scopes: dict[ScopeId, Scope]) -> None | Scope:
-    potential_new_scope = re.match(r"^\s*(.*?){\s*$", line)
-    if potential_new_scope is not None:
-        scope_name: str = potential_new_scope.group(1)
-        # group 1 can be empty for unamed blocks, just add a random name to it
-        if scope_name == "":
-            scope_name = "unnamed_scope_" + generate_random_string()
-        new_scope_id = ScopeId(scope_name)
-        new_scope = Scope(new_scope_id, current_scope.id, [])
-        return new_scope
-    else:
-        return None
+        letters = string.ascii_letters
+        return ''.join(random.choice(letters) for _ in range(length))
 
 
-def end_of_scope(line: str, current_scope: Scope) -> bool:
-    """
-    If end of scope, return the parent scope
-    Also print the scope and its variables before deleting it
-    """
-    potential_end_of_scope = re.match(r"^\s*}\s*$", line)
-    if potential_end_of_scope is not None:
-        print_scope(current_scope)
-        return True
-    else:
-        return False
+class Translator:
+    symbol_table: SymbolTable
 
-def print_scope(scope: Scope):
-    var_str = ", ".join([str(var) for var in scope.variables])
-    print(f"Scope ({scope}) variables:", var_str)
+    def __init__(self) -> None:
+        print("Static symbol table generation...")
+
+        with open(TEST_FILE) as f:
+            lines = f.readlines()
+
+        global_scope_id = ScopeId("global")
+        current_scope: Scope = Scope(global_scope_id, None, [])
+        symbol_table = SymbolTable({
+            global_scope_id: current_scope
+        })
+
+        for i in range(len(lines)):
+            line = lines[i]
+            self.variables_declaration(line, current_scope, i)
+            potential_new_scope = self.new_scope(line, current_scope, symbol_table.scopes)
+            potential_end_of_scope = self.end_of_scope(line, current_scope)
+            
+            if potential_new_scope is not None:
+                # add new scope to the current scope
+                current_scope = potential_new_scope
+                symbol_table.scopes[current_scope.id] = current_scope
+            elif potential_end_of_scope:
+                if current_scope.parent is not None:
+                    current_scope = symbol_table.scopes[current_scope.parent]
+
+        # print the remaining outermost scope
+        assert current_scope.id == global_scope_id
+        print(current_scope)
+
+        
+    def variables_declaration(self, line: str, current_scope: Scope, line_index: int):
+        # using re, check if string is of the form "type variable_name(, variable_name)*;"
+        # for every variable name, add it to the current scope
+        potential_var_declaration = re.match(r"^\s*(int|float|char|bool)\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*(,\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*)*;$", line)
+        if potential_var_declaration is not None:
+            variables_type = potential_var_declaration.group(1)
+            variables = re.split(r"\s*,\s*", potential_var_declaration.group(2))
+            if potential_var_declaration.group(3) is not None:
+                potential_additional_variables = re.sub(r"^\s*,\s*", "", potential_var_declaration.group(3))
+                variables.extend(re.split(r"\s*,\s*", potential_additional_variables))
+            for variable in variables:
+                current_scope.variables.append(VariableDeclaration(variable, variables_type, line_index + 1))
+
+
+    def new_scope(self, line: str, current_scope: Scope, scopes: dict[ScopeId, Scope]) -> None | Scope:
+        potential_new_scope = re.match(r"^\s*(.*?){\s*$", line)
+        if potential_new_scope is not None:
+            scope_name: str = potential_new_scope.group(1)
+            # group 1 can be empty for unamed blocks, just add a random name to it
+            if scope_name == "":
+                scope_name = "unnamed_scope_" + generate_random_string()
+            new_scope_id = ScopeId(scope_name)
+            new_scope = Scope(new_scope_id, current_scope.id, [])
+            return new_scope
+        else:
+            return None
+
+
+    def end_of_scope(self, line: str, current_scope: Scope) -> bool:
+        """
+        If end of scope, return the parent scope
+        Also print the scope and its variables before deleting it
+        """
+        potential_end_of_scope = re.match(r"^\s*}\s*$", line)
+        if potential_end_of_scope is not None:
+            print(current_scope)
+            return True
+        else:
+            return False
+
+
 
 def main():
-    print("Static symbol table generation...")
-
-    with open(TEST_FILE) as f:
-        lines = f.readlines()
-
-    global_scope_id = ScopeId("global")
-    current_scope: Scope = Scope(global_scope_id, None, [])
-    scopes: dict[ScopeId, Scope] = {
-        global_scope_id: current_scope
-    }
-
-    for i in range(len(lines)):
-        line = lines[i]
-        variables_declaration(line, current_scope, i)
-        potential_new_scope = new_scope(line, current_scope, scopes)
-        potential_end_of_scope = end_of_scope(line, current_scope)
-        
-        if potential_new_scope is not None:
-            # add new scope to the current scope
-            current_scope = potential_new_scope
-            scopes[current_scope.id] = current_scope
-        elif potential_end_of_scope:
-            if current_scope.parent is not None:
-                current_scope = scopes[current_scope.parent]
-
-    # print the remaining outermost scope
-    assert current_scope.id == global_scope_id
-    print_scope(current_scope)
+    Translator()
 
 
 # run the main function
